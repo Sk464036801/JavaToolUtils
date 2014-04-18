4fb79f48f4afbc2f27fe00f03e651ec3bb375fd6
Sk464036801 <xiaoshang_ke@sina.cn>
2014-04-19 00:04:34 +0800

    Java tool utilits initial .

---
+++ src/com/dev/tool/util/TestClass.java
@@ -0,0 +1,11 @@
+package com.dev.tool.util;
+
+/**
+ * Created by gk on 4/19/14.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("smart git submit test .");
+    }
+}
