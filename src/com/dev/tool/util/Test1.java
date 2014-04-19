8dc4fd6e5a9025a9ca65d5fc20175f729d4b3655
Sk464036801 <xiaoshang_ke@sina.cn>
2014-04-19 14:37:58 +0800

    Test1 类修改提交。

--- src/com/dev/tool/util/Test1.java
+++ src/com/dev/tool/util/Test1.java
@@ -4,4 +4,8 @@ package com.dev.tool.util;
  * Created by gk on 4/19/14.
  */
 public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("rebase Test1 class.");
+    }
 }
